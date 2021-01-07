1a07f4ed9b04025e9c6b260795993fc5e6dae3ca
Joni <yoanhristov9@gmail.com>
2021-01-07 19:22:07 +0200

    Fixed player king movement, added broken logic for computer king

--- Simple_chess.cpp
+++ Simple_chess.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <string>
 #include <ctime>
-#include <cstdlib>
+#include <cstdlib> 
 #include <string>
-using namespace std;
+using std::cout;
+using std::endl;
 
 char board[100][100];
 int xKing1, yKing1, xKing2, yKing2, xRook1, yRook1, xRook2, yRook2;
 int boardSize = 8, newSize;
 int turn = 1; //cpu playes first
-bool isRunning = 1, draw, mate;
+bool isRunning = 1, draw, mate, capturedR1, capturedR2;
 
 void changeBoardSize();
 void displayStartingScreen();
@@ -22,6 +23,8 @@ void genRook1();
 void genRook2();
 void generateFigures();
 void playerMove();
+void cpuMove();
+void cpuKingMove();
 
 int main()
 {
@@ -29,29 +32,38 @@ int main()
     do
     {
         displayStartingScreen();
-        cin >> option;
+        std::cin >> option;
         if (option == 1)
         {
             generateFigures();
-            printBoard();
+            //printBoard();
             while (draw != 1 || mate != 1)
             {
                 if (turn % 2 == 0)
                 {
-                    cout<<"King position "<<xKing1<<", "<<yKing1<<endl;
-                    cout<<"Rook1 position "<<xRook1<<", "<<yRook1<<endl;
-                    cout<<"Rook2 position "<<xRook2<<", "<<yRook2<<endl;
+                    printBoard();
+                    cout << "King position " << xKing1 << ", " << yKing1 << endl;
+                    cout << "Rook1 position " << xRook1 << ", " << yRook1 << endl;
+                    cout << "Rook2 position " << xRook2 << ", " << yRook2 << endl;
+                    cout << "Enemy King is at " << xKing2 << ", " << yKing2 << endl;
                     playerMove();
                     turn++;
-                    printBoard();
                 }
                 else
                 {
-                    //cpuMove();
+                    //printBoard();
+                    cpuMove();
                     turn++;
                 }
             }
-            //count turns
+            if (mate)
+            {
+                cout << "MATE in " << turn << "moves" << endl;
+            }
+            else if (draw)
+            {
+                cout << "STALEMATE in " << turn << "moves" << endl;
+            }
         }
         else if (option == 2)
         {
@@ -82,11 +94,11 @@ void displayStartingScreen()
 void changeBoardSize()
 {
     cout << "Enter the new board size" << endl;
-    cin >> newSize;
+    std::cin >> newSize;
     while (newSize == boardSize)
     {
         cout << "! Enter different board size !" << endl;
-        cin >> newSize;
+        std::cin >> newSize;
     }
     boardSize = newSize;
     cout << "Board size changed to: " << newSize << "x" << newSize << endl;
@@ -158,19 +170,19 @@ void printBoard()
     {
         for (int j = 0; j < boardSize; j++)
         {
-            if (xKing2 == i && yKing2 == j)
+            if (xKing2 == j && yKing2 == i)
             {
                 cout << "V";
             }
-            else if (xKing1 == i && yKing1 == j)
+            else if (xKing1 == j && yKing1 == i)
             {
                 cout << "K";
             }
-            else if (xRook1 == i && yRook1 == j)
+            else if (xRook1 == j && yRook1 == i)
             {
                 cout << "1";
             }
-            else if (xRook2 == i && yRook2 == j)
+            else if (xRook2 == j && yRook2 == i)
             {
                 cout << "2";
             }
@@ -189,22 +201,29 @@ void playerMove()
     int x, y;
     do
     {
-        // cout<<"King position "<<xKing1<<", "<<yKing1<<endl;
-        // cout<<"Rook1 position "<<xRook1<<", "<<yRook1<<endl;
-        // cout<<"Rook2 position "<<xRook2<<", "<<yRook2<<endl;
-        cout<<"Your turn, enter coordinates in format P X Y"<<endl;
-        cin>>figure>>x>>y;
+        if (capturedR1 && capturedR2)
+        {
+            draw = 1;
+            break;
+        }
+        
+        cout << "Your turn, enter coordinates in format P X Y" << endl;
+        std::cin >> figure >> x >> y;
         if (x < 0 || y < 0 || x >= boardSize || y >= boardSize)
         {
             cout << "Invalid coordinates" << endl;
         }
         else if (figure == 'K')
         {
-            if (x < xKing1 - 1 || y < yKing1 - 1 || x > xKing1 + 1 || y > yKing2 + 1)
+            if (x < (xKing1 - 1) || y < (yKing1 - 1) || x > (xKing1 + 1) || y > (yKing1 + 1))
             {
-                cout << "Invalid coordinates, king can only move 1 tile" << endl;
+                cout << "Invalid coordinates, the king can only move 1 tile" << endl;
             }
-            else if ((x >= xKing2 - 1 && x <= xKing2 + 1) && (y >= yKing2 - 1 && y <= yKing2 + 1) || x == xRook1 || y == yRook1 || x == xRook2 || y == yRook2)
+            else if (x == xKing1 && y == yKing1)
+            {
+                cout << "The king doesnt move" << endl;
+            }
+            else if ((x >= xKing2 - 1 && x <= xKing2 + 1) && (y >= yKing2 - 1 && y <= yKing2 + 1) || (x == xRook1 && y == yRook1) || (x == xRook2 && y == yRook2))
             {
                 cout << "Illegal move" << endl;
             }
@@ -213,13 +232,17 @@ void playerMove()
                 xKing1 = x;
                 yKing1 = y;
                 break;
-            }    
+            }
         }
         else if (figure == '1')
         {
-            if ((x == xKing1 && y == yKing1) || (x == xRook2 && y == yRook2))
+            if (capturedR1)
             {
-                cout<<"Illegal move, a piece has already occupied this square"<<endl;
+                cout << "This piece is no loger in play" << endl;
+            }
+            else if ((x == xKing1 && y == yKing1) || (x == xRook2 && y == yRook2))
+            {
+                cout << "Illegal move, a piece has already occupied this square" << endl;
             }
             else if ((x == xRook1 && y != yRook1) || (x != xRook1 && y == yRook1))
             {
@@ -234,9 +257,13 @@ void playerMove()
         }
         else if (figure == '2')
         {
-            if ((x == xKing1 && y == yKing1) || (x == xRook1 && y == yRook1))
+            if (capturedR2)
             {
-                cout<<"Illegal move, a piece has already occupied this square"<<endl;
+                cout << "This piece is no loger in play" << endl;
+            }
+            else if ((x == xKing1 && y == yKing1) || (x == xRook1 && y == yRook1))
+            {
+                cout << "Illegal move, a piece has already occupied this square" << endl;
             }
             else if ((x == xRook2 && y != yRook2) || (x != xRook2 && y == yRook2))
             {
@@ -255,4 +282,130 @@ void playerMove()
         }
 
     } while (true);
+}
+void cpuMove()
+{
+    //the king is in check
+    if (xKing2 == xRook1 || xKing2 == xRook2 || yKing2 == yRook1 || yKing2 == yRook2)
+    {
+        //if rook 1 is in play
+        if (capturedR1 == 0)
+        {
+            //check if king is next to rook1
+            if ((xRook1 >= xKing2 - 1 && xRook1 <= xKing2 + 1) && (yRook1 >= yKing2 - 1 && yRook1 <= yKing2 + 1))
+            {
+                //possible breaking below, check the king part
+                if (xRook1 != xRook2 && yRook1 != yRook2 && (xRook1 < xKing1 - 1 || xRook1 > xKing1 + 1 || xRook1 < yKing1 - 1 || xRook1 > yKing1 + 1))
+                {
+                    xKing2 = xRook1;
+                    yKing2 = yRook1;
+                    capturedR1 = 1;
+                    xRook1 = -1;
+                    yRook1 = -1;
+                }
+            }
+        }
+        //if rook2 is in play
+        else if (capturedR2 == 0)
+        {
+            //check if king is next to rook2
+            if ((xRook2 >= xKing2 - 1 && xRook2 <= xKing2 + 1) && (yRook2 >= yKing2 - 1 && yRook2 <= yKing2 + 1))
+            {
+                //possible breaking below, check the king part
+                if (xRook1 != xRook2 && yRook1 != yRook2 && (xRook2 < xKing1 - 1 || xRook2 > xKing1 + 1 || xRook2 < yKing1 - 1 || xRook2 > yKing1 + 1))
+                {
+                    xKing2 = xRook2;
+                    yKing2 = yRook2;
+                    capturedR1 = 1;
+                    xRook2 = -1;
+                    yRook2 = -1;
+                }
+            }
+        }
+        else
+        {
+            //move to safe
+            cpuKingMove();
+        }
+    }
+    //the king is not in check
+    else if (capturedR1 == 0)
+    {
+        //check if king is next to rook1
+        if ((xRook1 >= xKing2 - 1 && xRook1 <= xKing2 + 1) && (yRook1 >= yKing2 - 1 && yRook1 <= yKing2 + 1))
+        {
+            //possible breaking below, check the king part
+            if (xRook1 != xRook2 && yRook1 != yRook2 && (xRook1 < xKing1 - 1 || xRook1 > xKing1 + 1 || xRook1 < yKing1 - 1 || xRook1 > yKing1 + 1))
+            {
+                xKing2 = xRook1;
+                yKing2 = yRook1;
+                capturedR1 = 1;
+                xRook1 = -1;
+                yRook1 = -1;
+            }
+        }
+    }
+    else if (capturedR2 == 0)
+    {
+        //check if king is next to rook2
+        if ((xRook2 >= xKing2 - 1 && xRook2 <= xKing2 + 1) && (yRook2 >= yKing2 - 1 && yRook2 <= yKing2 + 1))
+        {
+            //possible breaking below, check the king part
+            if (xRook1 != xRook2 && yRook1 != yRook2 && (xRook2 < xKing1 - 1 || xRook2 > xKing1 + 1 || xRook2 < yKing1 - 1 || xRook2 > yKing1 + 1))
+            {
+                xKing2 = xRook2;
+                yKing2 = yRook2;
+                capturedR1 = 1;
+                xRook2 = -1;
+                yRook2 = -1;
+            }
+        }
+    }
+    //cant capture anything and we are not in danger
+    else 
+    {
+        cpuKingMove();
+    }
+}
+void cpuKingMove()
+{
+    if (xKing2 - 1 != xRook1 && xKing2 - 1 != xRook2 && xKing2 - 1 != xKing1 + 1 && xKing2 - 1 >= 0)
+    {
+        xKing2--;
+    }
+    else if (xKing2 + 1 != xRook1 && xKing2 + 1 != xRook2 && xKing2 + 1 != xKing1 - 1 && xKing2 + 1 < boardSize)
+    {
+        xKing2++;
+    }
+    else if (yKing2 - 1 != yRook1 && yKing2 - 1 != yRook2 && yKing2 - 1 != yKing1 + 1 && yKing2 - 1 >= 0)
+    {
+        yKing2--;
+    }
+    else if (yKing2 + 1 != yRook1 && yKing2 + 1 != yRook2 && yKing2 + 1 != yKing1 - 1 && yKing2 + 1 < boardSize)
+    {
+        yKing2++;
+    } /*
+    else if ((xKing2 - 1 != xRook1 && xKing2 - 1 != xRook2 && xKing2 - 1 != xKing1 + 1 && xKing2 - 1 >= 0) && (yKing2 - 1 != yRook1 && yKing2 - 1 != yRook2 && yKing2 - 1 != yKing1 + 1 && yKing2 - 1 >= 0))
+    {
+        xKing2--;
+        yKing2--;
+    }
+    else if ((xKing2 - 1 != xRook1 && xKing2 - 1 != xRook2 && xKing2 - 1 != xKing1 + 1 && xKing2 - 1 >= 0) && (yKing2 + 1 != yRook1 && yKing2 + 1 != yRook2 && yKing2 + 1 != yKing1 - 1 && yKing2 + 1 < boardSize))
+    {
+        xKing2--;
+        yKing2++;
+    }
+    else if ((xKing2 + 1 != xRook1 && xKing2 + 1 != xRook2 && xKing2 + 1 != xKing1 - 1 && xKing2 + 1 < boardSize) && (yKing2 - 1 != yRook1 && yKing2 - 1 != yRook2 && yKing2 - 1 != yKing1 + 1 && yKing2 - 1 >= 0))
+    {
+        xKing2++;
+
+    }
+    else if ((xKing2 + 1 != xRook1 && xKing2 + 1 != xRook2 && xKing2 + 1 != xKing1 - 1 && xKing2 + 1 < boardSize) && )
+    {
+        xKing2++;
+    }*/
+    else
+    {
+        mate = 1;
+    }
 }
